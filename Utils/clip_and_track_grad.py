cf338ea8c7aca3411ee47286a1e4cf2f87f1106f
colter <colter.norick@gmail.com>
2019-03-21 23:28:51 -0400

    changed clip bn value to better reflect the name

--- Utils/clip_and_track_grad.py
+++ Utils/clip_and_track_grad.py
@@ -8,8 +8,8 @@ def clip_and_track_grad(model, config):
     epsilon = pow(1/10, 3)
     clipped_params = {name: p.grad.data.clone().detach()
                       for name, p in model.named_parameters()}
-    # clip_grad_value_(model.parameters(), config.grad_clip)
-    clip_batch_norm_grad_value_(model.named_parameters(), config.grad_clip)
+    clip_grad_value_(model.parameters(), config.grad_clip)
+    # clip_batch_norm_grad_value_(model.named_parameters(), config.grad_clip)
     for name, param in model.named_parameters():
         clipped_params[name] -= param.grad.data
         if torch.max(clipped_params[name]) < epsilon or torch.min(clipped_params[name]) > -epsilon:
@@ -23,5 +23,5 @@ def clip_and_track_grad(model, config):
 def clip_batch_norm_grad_value_(named_params, clip_value):
 
     clip_value = float(clip_value)
-    for name, p in filter(lambda p: p[1].grad is not None or "bn" in p[0], named_params):
+    for name, p in filter(lambda p: p[1].grad is not None and "bn" in p[0], named_params):
         p.grad.data.clamp_(min=-clip_value, max=clip_value)
